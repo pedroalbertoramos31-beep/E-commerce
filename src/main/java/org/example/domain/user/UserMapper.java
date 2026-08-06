1e0b7fef2154f3d29966d0f6341178b1c8e0ba52
alberto <pedroalbertoramos31@gmail.com>
2026-08-06 16:47:05 -0700

    refactor(user): remove unused classes and methods

--- src/main/java/org/example/domain/user/UserMapper.java
+++ src/main/java/org/example/domain/user/UserMapper.java
@@ -1,15 +1,12 @@
 package org.example.domain.user;
 
 import org.example.domain.user.dto.response.UserBalanceResponse;
-import org.example.domain.user.dto.response.UserEntityResponse;
 import org.example.domain.user.dto.response.UserProfileResponse;
 import org.example.domain.user.dto.response.UserRoleChangeResponse;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
-    UserEntityResponse toUserResponse(User user);
 
     UserProfileResponse toProfileResponse(User user);
 
